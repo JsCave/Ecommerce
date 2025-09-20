1785c32d45c813260399d525d8024c80aad029cf
JsCave <67611356+JsCave@users.noreply.github.com>
2025-09-21 00:03:34 +0200

    Update page.tsx

--- src/app/(pages)/auth/login/page.tsx
+++ src/app/(pages)/auth/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm } from "react-hook-form"
 
 import {
   Form,
@@ -13,18 +13,19 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-
+import { signIn } from "next-auth/react"
+import { useRouter, useSearchParams } from "next/navigation"
+import { useState } from "react"
+import { Loader2 } from "lucide-react"
 
 const formSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 })
 
-
 type LoginFormValues = z.infer<typeof formSchema>
 
 export default function LoginPage() {
-
   const form = useForm<LoginFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -33,33 +34,32 @@ export default function LoginPage() {
     },
   })
 
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const callbackURL = searchParams.get("callbackUrl") || "/products"
+  const [isSigningIn, setIsSigningIn] = useState(false)
 
-  const onSubmit: SubmitHandler<LoginFormValues> = async (values) => {
-      setIsSigningIn(true)
-    console.log(values)
-    try{
-      const response=await signIn('credentials',{
-        email:values.email,
-        password:values.password,
-        redirect:false
+  async function onSubmit(values: LoginFormValues) {
+    setIsSigningIn(true)
+    try {
+      const response = await signIn("credentials", {
+        email: values.email,
+        password: values.password,
+        redirect: false,
       })
-      console.log(response)
-      if(response?.ok){
+      if (response?.ok) {
         router.push(callbackURL)
       }
+    } catch (e) {
+      console.error(e)
     }
-    catch(e){console.log(e)}
     setIsSigningIn(false)
-
   }
 
   return (
     <div className="max-w-2xl mx-auto my-12">
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8"
-        >
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
             control={form.control}
             name="email"
@@ -90,7 +90,8 @@ export default function LoginPage() {
             )}
           />
 
-          <Button type="submit" className="w-full">
+          <Button disabled={isSigningIn} type="submit" className="w-full">
+            {isSigningIn && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
             Login
           </Button>
         </form>
