0977d7a087bdb25901f533054c15e1a7f8729fcb
JsCave <67611356+JsCave@users.noreply.github.com>
2025-09-21 02:57:17 +0200

    Create page.tsx

---
+++ src/app/(pages)/verify/page.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { signIn } from "next-auth/react"
+import { useRouter, useSearchParams } from "next/navigation"
+import { useState } from "react"
+import { Loader2 } from "lucide-react"
+import { apiServices } from "@/services/api"
+
+
+const formSchema = z.object({
+  email: z.email("Invalid email address"),
+})
+
+type LoginFormValues = z.infer<typeof formSchema>
+
+export default function Verify() {
+
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: ""
+    },
+  })
+
+  const router = useRouter()
+ 
+
+  const [isLoading, setIsLoading] = useState(false)
+const[errors,setErrors]=useState<string>(""); 
+
+  async function onSubmit(values: LoginFormValues) {
+    setIsLoading(true)
+    try {
+      const response = await apiServices.forgetPassword(values.email)
+
+      console.log(response)
+      if (response?.statusMsg=='success') {
+        router.push('verify')
+      }
+      
+      if (response?.statusMsg=='success') {
+        router.push('verify')
+      }else{
+        setErrors(response.message)
+      }
+
+    } catch (e) {
+setErrors("An unexpected error occurred");
+      }
+    setIsLoading(false)
+  }
+
+  return (
+    <div className="max-w-2xl mx-auto my-12">
+        <div className="bg-green-400 text-green-700">Kindly insert verify code we sent to your email</div>
+        {errors && <div className="bg-red-400 text-red-700">{errors}</div>}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your email" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+
+          <Button disabled={isLoading} type="submit" className="w-full">
+            {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+            Reset Password
+          </Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
